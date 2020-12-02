4d79bf88ef95e5c710c5591c5d8cc9608fe46f80
tbremard <t.bremard@gmail.com>
2020-12-02 20:32:48 +0100

    is equal : done

---
+++ MySpector.UnitTest/NumberIsEqualCheckerTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+
+namespace MySpector.UnitTest
+{
+    public class NumberIsEqualCheckerTest
+    {
+        NumberIsEqualChecker _sut;
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [TestCase(true, 0, 0)]
+        [TestCase(true, 0.0001, 0.0001)]
+        [TestCase(false, 4, 5)]
+        [TestCase(true, 5, 5)]
+        [TestCase(false, 6, 5)]
+        [TestCase(false, -6, -5)]
+        [TestCase(true, -5, -5)]
+        [TestCase(true, -5.56, -5.56)]
+        [TestCase(false, -4.1, -5.1)]
+        public void Check_WenInputIsValid_ThenOk(bool expectedOutput, double sample, double reference)
+        {
+            _sut = new NumberIsEqualChecker(new decimal(sample), new decimal(reference));
+
+            var data = _sut.Check();
+
+            Assert.AreEqual(expectedOutput, data);
+        }
+    }
+
+}
---
+++ MySpector/NumberIsEqualChecker.cs
@@ -0,0 +1,22 @@
+﻿namespace MySpector
+{
+    public class NumberIsEqualChecker : IChecker
+    {
+        decimal Sample;
+        decimal Reference;
+
+        public NumberIsEqualChecker(decimal sample, decimal reference)
+        {
+            Sample = sample;
+            Reference = reference;
+        }
+
+        public bool Check()
+        {
+            bool ret;
+            ret = Sample == Reference;
+            return ret;
+        }
+    }
+
+}
