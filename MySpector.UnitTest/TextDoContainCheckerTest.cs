9ffa2785c089e47e8159542eb47d564ff6743ebe
tbremard <t.bremard@gmail.com>
2020-12-02 08:46:09 +0100

    TextDoContainCheckerTest

---
+++ MySpector.UnitTest/TextDoContainCheckerTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+
+namespace MySpector.UnitTest
+{
+    public class TextDoContainCheckerTest
+    {
+        TextDoContainChecker _sut;
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+
+        [TestCase(false, "zzzzzzzzzzzzzz", "yyy", true)]
+        [TestCase(true,  "zzzyyyzzzzzzzz", "yyy", true)]
+        [TestCase(true,  "zzzzzzzzzzzyyy", "yyy", true)]
+        [TestCase(false, "zzzzzzzzzzzYYY", "yyy", false)]
+        public void Check_WenInputIsValid_ThenOk(bool expectedOutput, string text, string token, bool ignoreCase)
+        {
+            _sut = new TextDoContainChecker(text, token, ignoreCase);
+
+            var data = _sut.Check();
+
+            Assert.AreEqual(expectedOutput, data);
+        }
+    }
+}
---
+++ MySpector/Checker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MySpector
+{
+    interface IChecker
+    {
+        public bool Check();
+    }
+
+    public class TextDoContainChecker : IChecker
+    {
+        string Text;
+        string Token;
+        bool IgnoreCase;
+
+        public TextDoContainChecker(string text, string token, bool ignoreCase)
+        {
+            Text = text;
+            Token = token;
+            IgnoreCase = ignoreCase;
+        }
+
+        public bool Check()
+        {
+            StringComparison comparisonType = IgnoreCase ? StringComparison.OrdinalIgnoreCase : StringComparison.Ordinal;
+            bool ret = Text.Contains(Token, comparisonType);
+            return ret;
+        }
+    }
+}
